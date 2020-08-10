f0904dff85d83d1db32f10e97c84315e929995e5
Jose Angel <jas@deb>
2020-08-10 09:48:22 -0400

    End of Magic 8 Ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -7,14 +8,9 @@ void main() => runApp(
       ),
     );
 
-class BallPage extends StatefulWidget {
-  BallPage({Key key}) : super(key: key);
+class BallPage extends StatelessWidget {
+  const BallPage({Key key}) : super(key: key);
 
-  @override
-  _BallPageState createState() => _BallPageState();
-}
-
-class _BallPageState extends State<BallPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +20,35 @@ class _BallPageState extends State<BallPage> {
         backgroundColor: Colors.blue[900],
         title: Text('Ask Me Anything'),
       ),
-      body: Container(),
+      body: Ball(),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  Ball({Key key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: getBallImages,
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+
+  void getBallImages() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+    //print('I got clicked $ballNumber');
   }
 }
